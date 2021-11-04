6dfc9e29c5df8d64aacda2e0c377123a96c801d3
Storm <storm.sarracino01@gmail.com>
2021-11-04 17:01:53 -0500

    commit tree 68f76820e68dc148c76741e48f44e00b2968b365

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate
+    case 
+    when a == b && a == c
+      :equilateral
+    when a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def greater_than_zero
+    a > 0 && b > 0 && c > 0
+  end
+
+  def triangle_inequality
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate
+    raise TriangleError unless greater_than_zero && triangle_inequality
+  end
+
+  class TriangleError < StandardError
+  end
 end
